042932766b8df8fc9153e2a4ed900590b69d87e8
nyn <nyn018@zjqunshuo.com>
2016-07-29 11:31:01 +0800

    golang state mode completely

--- sample/golang/state.go
+++ sample/golang/state.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 const (
@@ -16,11 +17,11 @@ const (
 	MOTOR_SIZE_MODE   = 3
 )
 
-type StateChangeFunc func(modeCode int)
+type StateChangeFunc func(modeCode int) int
 
 func main() {
 
-	StateMap := [MOTOR_SIZE_EVENT][MOTOR_SIZE_MODE]int{
+	StateTable := [MOTOR_SIZE_EVENT][MOTOR_SIZE_MODE]int{
 		/*state                0:MOTOR_MODE_STOP, 1:MOTOR_MODE_RUN,  2:MOTOR_MODE_ERROR*/
 		/*event*/
 		/*0 MOTOR_EVENT_STOP */ {MOTOR_MODE_STOP, MOTOR_MODE_STOP, MOTOR_MODE_ERROR},
@@ -28,7 +29,7 @@ func main() {
 		/*2 MOTOR_EVENT_ERROR*/ {MOTOR_MODE_ERROR, MOTOR_MODE_ERROR, MOTOR_MODE_ERROR},
 		/*3 MOTOR_EVENT_RESET*/ {MOTOR_MODE_STOP, MOTOR_MODE_ERROR, MOTOR_MODE_STOP}}
 
-	EventMap := [MOTOR_SIZE_EVENT][MOTOR_SIZE_MODE]*StateChangeFunc{
+	EventTable := [MOTOR_SIZE_EVENT][MOTOR_SIZE_MODE]StateChangeFunc{
 		/*state                0:MOTOR_MODE_STOP, 1:MOTOR_MODE_RUN,  2:MOTOR_MODE_ERROR*/
 		/*event*/
 		/*0 MOTOR_EVENT_STOP */ {motor_none, motor_stop, motor_none},
@@ -36,19 +37,58 @@ func main() {
 		/*2 MOTOR_EVENT_ERROR*/ {motor_error, motor_error, motor_none},
 		/*3 MOTOR_EVENT_RESET*/ {motor_reset, motor_error, motor_reset}}
 
+	//当前状态
+	currMode := MOTOR_MODE_STOP
+	eventSwith := make(chan int, 1)
+	for {
+		//产生事件
+		select {
+		case eventSwith <- MOTOR_EVENT_STOP:
+		case eventSwith <- MOTOR_EVENT_RUN:
+		case eventSwith <- MOTOR_EVENT_ERROR:
+		case eventSwith <- MOTOR_EVENT_RESET:
+		}
+		event := <-eventSwith
+		//引发动作
+		fmt.Println("motor is in " + getMode(currMode))
+		currMode = EventTable[event][currMode](StateTable[event][currMode])
+		time.Sleep(5 * time.Second)
+	}
+
 }
-func motor_run(modeCode int) {
-	fmt.Println("motor runs well")
+func motor_run(modeCode int) int {
+	fmt.Println("motor runs")
+	fmt.Println("motor change into " + getMode(modeCode))
+	return modeCode
 }
-func motor_stop(modeCode int) {
+func motor_stop(modeCode int) int {
 	fmt.Println("motor stop")
+	fmt.Println("motor change into " + getMode(modeCode))
+	return modeCode
 }
-func motor_error(modeCode int) {
-	fmt.Println("motor maybe has some problem")
+func motor_error(modeCode int) int {
+	fmt.Println("motor error")
+	fmt.Println("motor change into " + getMode(modeCode))
+	return modeCode
 }
-func motor_none(modeCode int) {
+func motor_none(modeCode int) int {
 	fmt.Println("motor do nothing")
+	fmt.Println("motor change into " + getMode(modeCode))
+	return modeCode
 }
-func motor_reset(modeCode int) {
+func motor_reset(modeCode int) int {
 	fmt.Println("moter reset")
+	fmt.Println("motor change into " + getMode(modeCode))
+	return modeCode
+}
+func getMode(modeCode int) string {
+	switch modeCode {
+	case MOTOR_MODE_STOP:
+		return "stop mode"
+	case MOTOR_MODE_RUN:
+		return "run mode"
+	case MOTOR_MODE_ERROR:
+		return "error mode"
+	}
+	return ""
 }
